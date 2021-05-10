84b54785ff99af671b4337f44ec5fc1d9d868015
Julie-ANANI <julie.anani@outlook.com>
2021-05-10 10:09:00 +0200

    transformation http en https

--- server/index.js
+++ server/index.js
@@ -1,7 +1,24 @@
 require('dotenv').config();
 
+const fs = require('fs');
+const path = require('path');
+const https =require('https');
 const express = require('express');
 const app = express();
+
+/**
+ * On récupère notre clé privée et notre certificat (ici ils se trouvent dans le dossier certificate)
+ * */
+const key = fs.readFileSync(path.join(__dirname,'certificate','server.key'));
+const cert = fs.readFileSync(path.join(__dirname,'certificate','server.cert'));
+ 
+const options = {key,cert};
+
+/* Puis on créer notre serveur HTTPS */
+https.createServer(options,app).listen(8080,()=>{
+    console.log('App is running ! Go to https://localhost:8080');
+});
+
 const PORT = process.env.PORT || 3000;
 
 const router = require('./app/router');
@@ -17,6 +34,12 @@ app.use((request, response, next) => {
 
 app.use('/v1/api/', router);
 
-app.listen(PORT, () => {
-    console.log(`Server running on : localhost:${PORT}/v1`)
+// app.listen(PORT, () => {
+//     console.log(`Server running on : localhost:${PORT}/v1`)
+// });
+
+
+/* Puis on créer notre serveur HTTPS */
+https.createServer(options,app).listen(PORT,()=>{
+          console.log(`App is running ! Go to https://localhost:${PORT}/v1`);
 });
