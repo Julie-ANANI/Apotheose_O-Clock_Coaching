7e88501f5e6355f5eccb6a330f288a0fdb304055
Julie-ANANI <julie.anani@outlook.com>
2021-06-24 23:51:23 +0200

    add config babel

---
+++ server/app.js
@@ -0,0 +1,66 @@
+//config app express.js
+import dotenv from 'dotenv'
+dotenv.config({ silent: true })
+import logger from 'morgan';
+import express from 'express';
+const expressSwagger = require('express-swagger-generator')(app);
+//indexRouter (router)
+import router from './app/router';
+
+const app = express();
+
+app.use(logger('dev'));
+
+const PORT = process.env.PORT || 3000;
+
+let options = {
+    swaggerDefinition: {
+
+        info: {
+            description: 'A coaching REST API',
+            title: 'Coaching',
+            version: '1.0.0',
+        },
+        host: `localhost:${PORT}`,
+        basePath: '/v1/api',
+        produces: [
+            "application/json",
+            "application/xml"
+        ],
+        schemes: ['http', 'https'],
+        securityDefinitions: {
+            JWT: {
+                type: 'apiKey',
+                in: 'header',
+                name: 'Authorization',
+                description: "",
+            }
+        }
+    },
+    basedir: __dirname, //app absolute path
+    files: [
+        './app/router.js',
+        './app/models/*.js'
+    ] //Path to the API handle folder
+};
+
+expressSwagger(options);
+
+// Middleware which parses incoming requests with JSON payloads
+app.use(express.json());
+
+// Allowing cross-origin requests in development
+// if (process.env.NODE_ENV === 'development') {
+app.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', 'http://localhost:8080');
+    res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization');
+    res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, DELETE');
+    res.header("Access-Control-Allow-Credentials", "true");
+    next();
+});
+// }
+
+
+app.use('/v1/api/', router);
+
+export default app;
